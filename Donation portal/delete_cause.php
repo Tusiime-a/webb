3fec365c50515f4838c3c707581f5e282e369bca
Tusiime <angelryan446@gmailcom>
2026-02-20 13:18:12 +0300

    Remove Donation portal folder

--- Donation portal/delete_cause.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-
-if ($_SESSION['username'] !== 'admin') die("Access denied");
-
-$id = $_GET['id'];
-$conn->query("DELETE FROM causes WHERE id=$id");
-
-header("Location: admin.php");
